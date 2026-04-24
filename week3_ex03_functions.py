fee702c9f45facdd4f76e212df849486a2ce4aee
Kamvelihle Matshaya <kayveemashaya@gmail.com>
2026-04-24 21:34:48 +0200

    Add Exercise 03 - Functions

---
+++ week3_ex03_functions.py
@@ -0,0 +1,38 @@
+# Question 1: Basic Function Definition and Calling
+
+# TODO: Define a function called 'greet' that prints "Hello, World!" 
+
+# TODO: Call the 'greet' function to see the output
+
+#----------------------------------------------------------------------------------------
+# Question 2: Function with Parameters
+
+# TODO: Define a function called 'personalized_greet' function with your name
+# TODO: Call the 'personalized_greet' function with your name
+ 
+# --------------------------------------------------------------------------------------
+
+# Question 3: Function with Return Value
+# TODO: Define a function called 'square' that takes a number as a parameter and returns the square of that number
+# TODO: Call the 'square' function with the number 5 and print the result
+
+# ---------------------------------------------------------------------------------------
+
+# Question 4: Function with Multiple Parameters and Return Value
+ 
+# TODO: Define a function called 'rectangle area' that takes length and width as parameters and returns the area of the rectangle
+ 
+# TODO: Call the 'rectangle_area' function with length 4 and width 5, and print the result
+ 
+# ---------------------------------------------------------------------------------------------------------------------------------
+
+# Question 5: Using a function as an Argument 
+ 
+# TODO: Define a function called 'apply_operation' that takes a function and a number as parameters, and returns the result of applying the function to the number
+ 
+# TODO: Define a function called 'double' that takes a number as a parameter and returns its double
+ 
+# TODO: Use the 'apply_operation' function with the 'double' function and the number 7, and print the result
+
+# TODO: Use the 'apply_operation' function with the 'square' function  (defined in Question 3) and the number 3, and print the result
+          
